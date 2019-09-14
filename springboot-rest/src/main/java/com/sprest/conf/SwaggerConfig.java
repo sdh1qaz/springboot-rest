307a2353b84816ab11a2972597405e943b394cd7
aa <aa@DESKTOP-K4U6T6K>
2019-09-15 01:05:34 +0800

    集成swagger

---
+++ springboot-rest/src/main/java/com/sprest/conf/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package com.sprest.conf;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	
+	@Value("${rest.env}")
+	private String env;
+	
+	@Value("${rest.server.addr}")
+	private String serverAddr;
+	
+	@Value("${rest.server.port}")
+	private String serverPort;
+	
+	
+	@Bean
+	public Docket configSpringfoxDocketForAll() {
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).
+				select().apis(RequestHandlerSelectors.basePackage("com.sprest"))
+				.paths(PathSelectors.any()).build();
+	}
+	
+	@SuppressWarnings("deprecation")
+	private ApiInfo apiInfo() {
+		return new ApiInfo("springboot-rest接口系统", 
+				"部署信息"+serverAddr+":"+serverPort, "1.0.0", "", "苏登辉", "", "");
+	}
+
+}
--- springboot-rest/src/main/java/com/sprest/controller/UserVoController.java
+++ springboot-rest/src/main/java/com/sprest/controller/UserVoController.java
@@ -10,6 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.sprest.pojo.UserVo;
 import com.sprest.service.IUserVoService;
 
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
 public class UserVoController {
 
@@ -19,6 +24,9 @@ public class UserVoController {
 	/**
 	 * 返回一个UserVo
 	 */
+	@ApiOperation(value="根据uid查用户信息",notes="根据uid查用户信息",httpMethod="GET")
+	@ApiResponses({@ApiResponse(code=200,message="success",response=UserVo.class)})
+	@ApiImplicitParams({})
 	@RequestMapping(path="/user/{uid}",method=RequestMethod.GET)
 	public @ResponseBody UserVo getUser(@PathVariable("uid") Integer uid) {
 
