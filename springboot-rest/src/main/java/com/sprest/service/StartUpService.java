3ee89c02d6ff21ccf3dc376e9464cd7a06f208e5
aa <aa@DESKTOP-K4U6T6K>
2019-10-02 04:00:11 +0800

    启动任务和定时任务

---
+++ springboot-rest/src/main/java/com/sprest/service/StartUpService.java
@@ -0,0 +1,24 @@
+package com.sprest.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Service;
+
+/**
+ * 小书包运行前启动项
+ * @author 苏登辉
+ */
+@Service
+public class StartUpService implements CommandLineRunner{
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(StartUpService.class);
+	
+	@Override
+	public void run(String... args) throws Exception {
+		LOGGER.info("启动时任务开始...");
+		//做任务
+		LOGGER.info("启动时任务结束...");
+	}
+	
+}
